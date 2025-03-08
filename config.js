cb925027335b0117c940ca4e723c81d43bf0452c
sale165 <whatsappbotsal@gmail.com>
2025-03-09 01:12:26 +0500

    config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "enter your session",
+    SESSION_ID: process.env.SESSION_ID || "SILENT-SOBX-MD~KYpmhShC#4t9EtBzpEZ8vx0-bxKJyhVh8nZH44rkwzbidfBH3nC0",
     CAPTION: process.env.CAPTION || "POWERED BY SILENTLOVER432",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "true",
